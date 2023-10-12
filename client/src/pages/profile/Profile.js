f6d4ffe897f8885716bba3f6419a4dfa2f4bfd44
AneezAmja <aneez.amja@gmail.com>
2023-10-12 14:36:12 +0100

    Profile.js: fixed user profile checking & added email updating

--- client/src/pages/profile/Profile.js
+++ client/src/pages/profile/Profile.js
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import "./Profile.css";
-import { getUser, uploadAvatar } from "../../features/user/userSlice";
+import {
+  getUser,
+  uploadAvatar,
+  updateUserEmail,
+} from "../../features/user/userSlice";
 import { FaUpload } from "react-icons/fa";
 import Spinner from "../../components/spinner/Spinner";
 
-
 import { toast } from "react-toastify";
-
 
 const Profile = () => {
   const dispatch = useDispatch();
@@ -17,8 +19,15 @@ const Profile = () => {
     (state) => state.user
   );
 
-
   // const [email, setEmail] = useState(user.email);
+
+  const [formData, setFormData] = useState({
+    email: user ? user.email : "",
+    password: "",
+  });
+
+  const { email, password } = formData; //destructring
+
   const [file, setFile] = useState(null);
 
   const handleFileChange = (event) => {
@@ -26,36 +35,44 @@ const Profile = () => {
   };
 
   const handleUpload = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
-    const formData = new FormData();
-    formData.append("avatar", file);
+    const imageFormData = new FormData();
+    imageFormData.append("avatar", file);
 
-    dispatch(uploadAvatar(formData)).then(()=>{
+    dispatch(uploadAvatar(imageFormData)).then(() => {
       dispatch(getUser());
-    })
-    
-    };
-    
-    useEffect(() => {
+    });
+  };
 
-      if (isError) {
-        toast.error(message);
-      }
-  
-      dispatch(getUser());
+  useEffect(() => {
+    if (isError) {
+      toast.error(message);
+    }
 
-  }, [user.profileImageURL, isError, isSuccess, message]);
+    dispatch(getUser());
+  }, [isError, isSuccess, message, dispatch]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
   };
 
   const handleTyping = (e) => {
-    // setEmail((prevState) => ({
-    //   ...prevState,
-    //   [e.target.name]: e.target.value,
-    // }));
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const updateCredentials = () => {
+    try {
+      dispatch(updateUserEmail({ updatedEmail: email })).then(() => {
+        dispatch(getUser());
+      });
+      toast.success("Email updated successfully.");
+    } catch (error) {
+      toast.error("Error updating email.");
+    }
   };
 
   if (isLoading) {
@@ -64,24 +81,25 @@ const Profile = () => {
 
   return (
     <div className="profile-container">
-      {/* <div><h3>welcome back {user.name}!</h3></div> */}
       <div className="profile-container__left">
         <div className="profile-image-container">
-          {user && !user.profileImageURL || user.profileImageURL === "" ? (
-            <div className="profile-image--default">
-              {user.name.charAt(0).toUpperCase()}
-            </div>
-          ) : (
-
+          {user && user.profileImageURL ? (
             <img
               className="profile-image"
-              src={`uploads/${user.profileImageURL}` || "https://static.wikia.nocookie.net/shingekinokyojin/images/a/a1/Eren_Jaeger_%28Anime%29_character_image.png/revision/latest?cb=20220123225500"}
+              src={`uploads/${user?.profileImageURL}`}
               alt=""
             />
+          ) : (
+            <div className="profile-image--default">
+              {user && user?.name ? user.name.charAt(0).toUpperCase() : ""}
+            </div>
           )}
         </div>
-        {/* <button className="upload-image">upload new picture</button> */}
-        <form action="/upload-avatar" method="post" encType="multipart/form-data">
+        <form
+          action="/upload-avatar"
+          method="post"
+          encType="multipart/form-data"
+        >
           <div className="file-input-container">
             <FaUpload fill="#1d1d1d" />
             <input
@@ -91,9 +109,8 @@ const Profile = () => {
               onChange={handleFileChange}
             />
           </div>
-          {/* <input type="submit" className="save-image" onClick={handleUpload}/> */}
           <button className="save-image" onClick={handleUpload}>
-            submit
+            Submit
           </button>
         </form>
       </div>
@@ -101,15 +118,27 @@ const Profile = () => {
         <form className="form" onSubmit={handleSubmit}>
           <div className="email-container">
             <label className="email">Change email</label>
-            <input type="text" />
-            {/* <input type="text" value={email} onChange={handleTyping} /> */}
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={email}
+              onChange={handleTyping}
+            />
           </div>
           <div className="password-container">
             <label className="password">Change password</label>
-            <input type="text" />
+            <input
+              type="password"
+              placeholder="Password"
+              id="password"
+              name="password"
+              value={password}
+              onChange={handleTyping}
+            />
           </div>
 
-          <button className="confirm-button" type="submit">
+          <button className="confirm-button" onClick={updateCredentials}>
             Save changes
           </button>
         </form>
