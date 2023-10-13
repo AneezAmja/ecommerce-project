38af11e96de58b0d2bc245572c51c21d46aabba2
AneezAmja <aneez.amja@gmail.com>
2023-10-13 20:03:53 +0100

    productService: add: created service to get all products

---
+++ client/src/features/product/productService.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const API_URL = "/api/products";
+
+// Get all products
+const getProducts = async () => {
+  const response = await axios.get(API_URL + `/`);
+
+  return response.data;
+};
+
+
+
+const productService = {
+  getProducts,
+};
+
+export default productService;
