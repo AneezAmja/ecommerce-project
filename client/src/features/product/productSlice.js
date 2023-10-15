a270eae59e9beca3d36770709863dc502612cc50
AneezAmja <aneez.amja@gmail.com>
2023-10-15 19:16:55 +0100

    add: adding getting individual product as a slice

--- client/src/features/product/productSlice.js
+++ client/src/features/product/productSlice.js
@@ -4,6 +4,7 @@ import productService from "./productService";
 
 const initialState = {
   products: [],
+  product: {},
   isError: null,
   isSuccess: null,
   isLoading: null,
@@ -16,6 +17,25 @@ export const getProducts = createAsyncThunk(
   async (thunkAPI) => {
     try {
       return await productService.getProducts();
+    } catch (error) {
+        console.log(error)
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+
+// Get an individual product
+export const getIndividualProduct = createAsyncThunk(
+  "product/getIndividualProduct",
+  async (id, thunkAPI) => {
+    try {
+      return await productService.getIndividualProduct(id);
     } catch (error) {
         console.log(error)
       const message =
@@ -56,6 +76,20 @@ export const productSlice = createSlice({
         state.isError = true;
         state.message = action.payload;
         state.products = null;
+      })
+      .addCase(getIndividualProduct.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getIndividualProduct.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.product = action.payload;
+      })
+      .addCase(getIndividualProduct.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+        state.product = null;
       })
       
   },
