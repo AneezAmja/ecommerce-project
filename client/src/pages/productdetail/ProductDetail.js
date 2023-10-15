b49e929529f307e115d73dd63cbe119d7470b370
AneezAmja <aneez.amja@gmail.com>
2023-10-15 19:17:28 +0100

    add: dispatching slice

--- client/src/pages/productdetail/ProductDetail.js
+++ client/src/pages/productdetail/ProductDetail.js
@@ -3,62 +3,67 @@ import axios from "axios";
 import { useParams } from "react-router-dom";
 import './ProductDetail.css'
 import Spinner from "../../components/spinner/Spinner";
-
-const API_URL = "/api/products/";
+import { toast } from "react-toastify";
+import { useSelector, useDispatch } from "react-redux";
+import { getIndividualProduct } from "../../features/product/productSlice";
+import { getUser } from "../../features/user/userSlice";
 
 const ProductDetail = () => {
-  const [item, setItem] = useState([]);
-  const [loading, setLoading] = useState(true);
+  
+  const { product, isError, isLoading, message } = useSelector(
+    (state) => state.products
+  );
 
   const { id } = useParams();
 
+  const dispatch = useDispatch();
+
   useEffect(() => {
-    getProductDetail();
-  }, []);
 
-  /* /api/products/:id */
-  const getProductDetail = async () => {
-    const response = await axios.get(API_URL + `${id}`);
-    const productDetailData = response.data;
+    
+    dispatch(getUser())
+    dispatch(getIndividualProduct(id))
 
-    // console.log(productDetailData);
+    if (isError) {
+      toast.error(message);
+    }
 
-    setItem(productDetailData);
-    setLoading(false);
-  };
+  }, [isError, message, dispatch]);
+
+
 
   const addToBasket = () => {
     console.log("added to basket")
   }
 
 
-  if (loading) return <Spinner/>;
+  if (isLoading) {
+    return <Spinner />;
+  }
 
   return (
     <div className="product-detail-container">
       <div className="product-detail">
 
-            <div className="product-detail__image-container" key={item._id}>
+            <div className="product-detail__image-container" key={product?._id}>
               <img
-                src={item.imageURL}
-                alt={item.name}
+                src={product?.imageURL}
+                alt={product?.name}
                 className="product-detail__image"
               />
             </div>
 
             <div className="product-detail__info">
-                <h2 className="product-detail__name">{item.name}</h2>
-                <p className="product-detail__price">£{item.price}</p>
-                <p className="product-detail__description">{item.productDetail.description}</p>
-                <p className="product-detail__stock"><i style={{opacity: '0.8'}}>stock quantity: </i><strong>{item.productDetail.stockQuantity}</strong></p>
+                <h2 className="product-detail__name">{product?.name}</h2>
+                <p className="product-detail__price">£{product?.price}</p>
+                <p className="product-detail__description">{product?.productDetail?.description}</p>
+                <p className="product-detail__stock"><i style={{opacity: '0.8'}}>stock quantity: </i><strong>{product?.productDetail?.stockQuantity}</strong></p>
 
                 <div className="product-detail-buy-container">
                   <div className="quantity-input">    
                     <input className="quantity-amount" type="number" id="quantity" name="quantity" placeholder="0" min="1" />
                   </div>
-                    {/* <a href='#' className="product-detail-buy-container__buy">
-                        Buy product
-                    </a> */}
+
                     <button onClick={addToBasket} className="product-detail-buy-container__buy">
                         Add to basket 
                     </button>
