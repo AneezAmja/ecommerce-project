348cb392e990833cd6e4197f5fa148f1c1a06586
AneezAmja <aneez.amja@gmail.com>
2023-11-08 01:47:57 +0000

    updated product store, added cart store

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from '../features/user/userSlice'
 import productReducer from '../features/product/productSlice'
+import cartReducer from '../features/cart/cartSlice'
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
-    products: productReducer
+    products: productReducer,
+    cart: cartReducer
   },
 });
