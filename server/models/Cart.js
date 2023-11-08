0960ee298b9abafcafbf134eaf90207cbe1c1ad2
AneezAmja <aneez.amja@gmail.com>
2023-11-08 01:43:11 +0000

    created Cart model

---
+++ server/models/Cart.js
@@ -0,0 +1,61 @@
+const mongoose = require("mongoose");
+
+const cartSchema = mongoose.Schema(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    items: [
+      {
+        productId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "ProductDetail",
+          required: true,
+        },
+        productName: {
+          type: mongoose.Schema.Types.String,
+          ref: "ProductDetail",
+          required: true,
+        },
+        productImage: {
+          type: mongoose.Schema.Types.String,
+          ref: "ProductDetail",
+          required: true,
+        },
+        productPrice: {
+          type: mongoose.Schema.Types.Number,
+          ref: "ProductDetail",
+          required: true,
+        },
+        quantity: { type: Number, default: 1 },
+      },
+    ],
+    createdAt: { type: Date, default: Date.now },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// -------- THE WAY I WANT TO DO --------
+// const cartSchema = mongoose.Schema(
+//   {
+//     user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+//     items: [
+//       {
+//         productId: {
+//           type: mongoose.Schema.Types.ObjectId,
+//           ref: "ProductDetail",
+//           required: true,
+//         },
+//         quantity: { type: Number, default: 1 },
+//       },
+//     ],
+//     createdAt: { type: Date, default: Date.now },
+//   },
+//   {
+//     timestamps: true,
+//   }
+// );
+
+const Cart = mongoose.model("Cart", cartSchema);
+
+module.exports = Cart;
