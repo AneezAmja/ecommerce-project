be93f3af6bebe4c54fbe64495882e8d96cefbcff
AneezAmja <aneez.amja@gmail.com>
2023-11-08 01:47:06 +0000

    created CartIcon

---
+++ client/src/components/cartIcon/CartIcon.js
@@ -0,0 +1,50 @@
+import { useEffect } from "react";
+import { FaShoppingCart } from "react-icons/fa";
+import "./CartIcon.css";
+import { getCart } from "../../features/cart/cartSlice";
+import { useSelector, useDispatch } from "react-redux";
+import { toast } from "react-toastify";
+
+import Spinner from "../../components/spinner/Spinner";
+
+const Cart = () => {
+  const dispatch = useDispatch();
+
+  const { cart, isError, isSuccess, message } = useSelector(
+    (state) => state.cart
+  );
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message);
+    }
+
+    dispatch(getCart())
+  }, [isError, dispatch]);
+
+  const getTotalQuantity = (cart) => {
+    let totalQuantity = 0;
+  
+    if (cart && cart[0] && cart[0].items) {
+      totalQuantity = cart[0].items.reduce((accumulator, item) => accumulator + item.quantity, 0);
+    }
+  
+    return totalQuantity;
+  };
+
+  const totalQuantity = getTotalQuantity(cart);
+
+  return (
+    <div className="shopping-cart__container">
+      <FaShoppingCart
+        className="shopping-cart"
+        style={{ marginRight: "30px", width: "20px", height: "20px" }}
+      />
+      <div className="shopping-cart--counter">
+        {totalQuantity}
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
