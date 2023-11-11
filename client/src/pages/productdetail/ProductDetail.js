f48faa36007c24cd892c4563a9bfe27feea06e77
AneezAmja <aneez.amja@gmail.com>
2023-11-11 23:22:49 +0000

    dispatching slice in order for purchases to be made

--- client/src/pages/productdetail/ProductDetail.js
+++ client/src/pages/productdetail/ProductDetail.js
@@ -1,41 +1,48 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
-import './ProductDetail.css'
+import "./ProductDetail.css";
 import Spinner from "../../components/spinner/Spinner";
 import { toast } from "react-toastify";
 import { useSelector, useDispatch } from "react-redux";
-import { getIndividualProduct } from "../../features/product/productSlice";
+import {
+  getIndividualProduct,
+  purchaseProduct,
+} from "../../features/product/productSlice";
 import { getUser } from "../../features/user/userSlice";
 
 const ProductDetail = () => {
-  
   const { product, isError, isLoading, message } = useSelector(
     (state) => state.products
   );
+
+  const [quantity, setQuantity] = useState(0);
 
   const { id } = useParams();
 
   const dispatch = useDispatch();
 
   useEffect(() => {
-
-    
-    dispatch(getUser())
-    dispatch(getIndividualProduct(id))
+    dispatch(getUser());
+    dispatch(getIndividualProduct(id));
 
     if (isError) {
-      toast.error(message);
+      toast.error('Please register or login before making a purchase!');
     }
-
   }, [isError, message, dispatch]);
 
+  const purchaseAmmount = () => {
+    const productInfo = {
+      productId: id,
+      quantity,
+    };
 
+    dispatch(purchaseProduct(productInfo)).then(
+      dispatch(getIndividualProduct(id))
+    );
 
-  const addToBasket = () => {
-    console.log("added to basket")
-  }
-
+    console.log("added to basket");
+  };
 
   if (isLoading) {
     return <Spinner />;
@@ -44,32 +51,47 @@ const ProductDetail = () => {
   return (
     <div className="product-detail-container">
       <div className="product-detail">
+        <div className="product-detail__image-container" key={product?._id}>
+          <img
+            src={product?.imageURL}
+            alt={product?.name}
+            className="product-detail__image"
+          />
+        </div>
 
-            <div className="product-detail__image-container" key={product?._id}>
-              <img
-                src={product?.imageURL}
-                alt={product?.name}
-                className="product-detail__image"
+        <div className="product-detail__info">
+          <h2 className="product-detail__name">{product?.name}</h2>
+          <p className="product-detail__price">£{product?.price}</p>
+          <p className="product-detail__description">
+            {product?.productDetail?.description}
+          </p>
+          <p className="product-detail__stock">
+            <i style={{ opacity: "0.8" }}>stock quantity: </i>
+            <strong>{product?.productDetail?.stockQuantity}</strong>
+          </p>
+
+          <div className="product-detail-buy-container">
+            <div className="quantity-input">
+              <input
+                className="quantity-amount"
+                type="number"
+                id="quantity"
+                name="quantity"
+                placeholder="0"
+                min="1"
+                value={quantity}
+                onChange={(e) => setQuantity(e.target.value)}
               />
             </div>
 
-            <div className="product-detail__info">
-                <h2 className="product-detail__name">{product?.name}</h2>
-                <p className="product-detail__price">£{product?.price}</p>
-                <p className="product-detail__description">{product?.productDetail?.description}</p>
-                <p className="product-detail__stock"><i style={{opacity: '0.8'}}>stock quantity: </i><strong>{product?.productDetail?.stockQuantity}</strong></p>
-
-                <div className="product-detail-buy-container">
-                  <div className="quantity-input">    
-                    <input className="quantity-amount" type="number" id="quantity" name="quantity" placeholder="0" min="1" />
-                  </div>
-
-                    <button onClick={addToBasket} className="product-detail-buy-container__buy">
-                        Add to basket 
-                    </button>
-                </div>
-            </div>
-
+            <button
+              onClick={purchaseAmmount}
+              className="product-detail-buy-container__buy"
+            >
+              Purchase ammount
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
