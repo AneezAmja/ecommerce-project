fb906450888d5beb744a2eebe6933cef33656fcf
AneezAmja <aneez.amja@gmail.com>
2023-11-28 14:52:01 +0000

    implementing scss

--- client/src/pages/productdetail/ProductDetail.js
+++ client/src/pages/productdetail/ProductDetail.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
-import './ProductDetail.css'
+import "./ProductDetail.scss";
 import Spinner from "../../components/spinner/Spinner";
 import { toast } from "react-toastify";
 import { useSelector, useDispatch } from "react-redux";
@@ -10,42 +10,35 @@ import { getUser } from "../../features/user/userSlice";
 import { addToCart, getCart } from "../../features/cart/cartSlice";
 
 const ProductDetail = () => {
-  
   const { product, isError, isLoading, message } = useSelector(
     (state) => state.products
   );
 
-  const [quantity, setQuantity] = useState(0)
+  const user = JSON.parse(localStorage.getItem("user"));
 
+  const [quantity, setQuantity] = useState(0);
   const { id } = useParams();
 
   const dispatch = useDispatch();
 
   useEffect(() => {
-
-    dispatch(getUser())
-    dispatch(getIndividualProduct(id))
+    dispatch(getUser());
+    dispatch(getIndividualProduct(id));
 
     if (isError) {
       toast.error(message);
     }
-
   }, [isError, message, dispatch, id]);
 
-
-
   const addToBasket = () => {
-
     const cartData = {
-      productId: product.productDetail._id,
-      quantity
-    }
+      productId: product._id,
+      quantity,
+    };
 
-
-    dispatch(addToCart(cartData)).then(()=> dispatch(getCart()))
-    console.log("added to basket")
-  }
-
+    dispatch(addToCart(cartData)).then(() => dispatch(getCart()));
+    console.log("added to basket");
+  };
 
   if (isLoading) {
     return <Spinner />;
@@ -54,32 +47,53 @@ const ProductDetail = () => {
   return (
     <div className="product-detail-container">
       <div className="product-detail">
+        <div className="product-detail__image-container" key={product?._id}>
+          <img
+            src={product?.imageURL}
+            alt={product?.name}
+            className="product-detail__image"
+          />
+        </div>
 
-            <div className="product-detail__image-container" key={product?._id}>
-              <img
-                src={product?.imageURL}
-                alt={product?.name}
-                className="product-detail__image"
+        <div className="product-detail__info">
+          <h2 className="product-detail__name">{product?.name}</h2>
+          <p className="product-detail__price">£{product?.price}</p>
+          <p className="product-detail__description">
+            {product?.productDetail?.description}
+          </p>
+          <p className="product-detail__stock">
+            <i style={{ opacity: "0.8" }}>stock quantity: </i>
+            <strong>{product?.productDetail?.stockQuantity}</strong>
+          </p>
+
+          <div className="product-detail-buy-container">
+            <div className="quantity-input">
+              <input
+                className="quantity-amount"
+                type="number"
+                id="quantity"
+                name="quantity"
+                placeholder="0"
+                min="1"
+                value={quantity}
+                onChange={(e) => setQuantity(e.target.value)}
               />
             </div>
 
-            <div className="product-detail__info">
-                <h2 className="product-detail__name">{product?.name}</h2>
-                <p className="product-detail__price">£{product?.price}</p>
-                <p className="product-detail__description">{product?.productDetail?.description}</p>
-                <p className="product-detail__stock"><i style={{opacity: '0.8'}}>stock quantity: </i><strong>{product?.productDetail?.stockQuantity}</strong></p>
-
-                <div className="product-detail-buy-container">
-                  <div className="quantity-input">    
-                    <input className="quantity-amount" type="number" id="quantity" name="quantity" placeholder="0" min="1" value={quantity} onChange={e => setQuantity(e.target.value)} />
-                  </div>
-
-                    <button onClick={addToBasket} className="product-detail-buy-container__buy">
-                        Add to basket 
-                    </button>
-                </div>
-            </div>
-
+            {user ? (
+              <button
+                onClick={addToBasket}
+                className="product-detail-buy-container__buy"
+              >
+                Add to basket
+              </button>
+            ) : (
+              <button className="product-detail-buy-container__buy" disabled>
+                Login to add to basket
+              </button>
+            )}
+          </div>
+        </div>
       </div>
     </div>
   );
