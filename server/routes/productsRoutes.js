2c8418e6d33cf21475ee238ecdfc335abd770341
AneezAmja <aneez.amja@gmail.com>
2023-11-28 15:29:33 +0000

    added new route for purchasing + protect

--- server/routes/productsRoutes.js
+++ server/routes/productsRoutes.js
@@ -1,11 +1,14 @@
 const express = require("express");
-const { getProducts, getIndividualProduct, purchaseProduct, updateProduct } = require("../controllers/productsController");
+const { getProducts, getIndividualProduct, purchaseProducts, updateProduct } = require("../controllers/productsController");
 const router = express.Router();
+const { protect } = require("../middleware/authMiddleware");
 
 // /api/products/
 router.route("/").get(getProducts)
 router.route("/:id").get(getIndividualProduct)
-router.route("/purchase").post(purchaseProduct)
+// router.route("/purchase").post(purchaseProducts)
+router.post("/purchase", protect, purchaseProducts);
+
 // router.route("/purchase/:productId").post(purchaseProduct)
 router.route("/update-stock").post(updateProduct)
 
