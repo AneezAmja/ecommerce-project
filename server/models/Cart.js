dd756cf4e09662948d9082ae5f233bd669f9906b
AneezAmja <aneez.amja@gmail.com>
2023-11-28 15:30:15 +0000

    removed comment

--- server/models/Cart.js
+++ server/models/Cart.js
@@ -35,27 +35,6 @@ const cartSchema = mongoose.Schema(
   }
 );
 
-// -------- THE WAY I WANT TO DO --------
-// const cartSchema = mongoose.Schema(
-//   {
-//     user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-//     items: [
-//       {
-//         productId: {
-//           type: mongoose.Schema.Types.ObjectId,
-//           ref: "ProductDetail",
-//           required: true,
-//         },
-//         quantity: { type: Number, default: 1 },
-//       },
-//     ],
-//     createdAt: { type: Date, default: Date.now },
-//   },
-//   {
-//     timestamps: true,
-//   }
-// );
-
 const Cart = mongoose.model("Cart", cartSchema);
 
 module.exports = Cart;
