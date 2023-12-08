2bd1f0093b15365a2c1a5e104dde8d70c5366552
AneezAmja <aneez.amja@gmail.com>
2023-12-08 00:51:26 +0000

    idnex.js: add: cors

--- server/index.js
+++ server/index.js
@@ -13,6 +13,7 @@ const app = express();
 //MIDDLEWARE
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
+app.use(cors());
 app.use('/assets', express.static(path.join(__dirname, 'data', 'assets')));
 app.use('/uploads/images/avatars', express.static(path.join(__dirname, 'public', 'images', 'avatars')));
 
