d34733f543e989599d2e92948006817a9f743816
AneezAmja <aneez.amja@gmail.com>
2023-12-12 18:45:13 +0000

    reordered cors

--- server/index.js
+++ server/index.js
@@ -12,9 +12,9 @@ const app = express();
 
 
 //MIDDLEWARE
+app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
-app.use(cors());
 app.use('/assets', express.static(path.join(__dirname, 'data', 'assets')));
 app.use('/uploads/images/avatars', express.static(path.join(__dirname, 'public', 'images', 'avatars')));
 
