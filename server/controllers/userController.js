1fe4c3b178c91d138b1b018f39b621f45dd9b6a7
AneezAmja <aneez.amja@gmail.com>
2024-02-28 01:07:23 +0000

    improved avatar quality

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -171,6 +171,7 @@ const uploadUserAvatar = asyncHandler(async (req, res) => {
         transformation: [
           { width: 200, height: 200, crop: 'fill', gravity: 'face' },
           { format: 'webp' },
+          { quality: 'auto' , fetch_format: "auto"},
         ],
       });
 
